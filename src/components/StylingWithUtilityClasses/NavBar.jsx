5c71c69aa38664520cc1642dec9f8927eab20cea
Kevin Langat <kevinlangat419@gmail.com>
2025-09-19 01:28:44 +0300

    Finalized the hover and other states

--- src/components/StylingWithUtilityClasses/NavBar.jsx
+++ src/components/StylingWithUtilityClasses/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import image from '../../assets/react.svg';
 
 function NavBar() {
@@ -5,15 +6,19 @@ function NavBar() {
     <div className=" w-full shadow px-4 h-12 flex flex-row items-center justify-between">
       <img src={image} alt="react" />
       <div className=" hidden group md:flex lg:w-1/2 sm:w-3/4 font-light h-1/2 rounded-full px-3 bg-gray-200 text-[0.77em]  flex-row  items-center justify-around">
-        <a href="/utility-classes">Utility Classes</a>
-        <a href="/hover-state">Hover</a>
-        <a href="">Focus</a>
-        <a href="">Dark Mode</a>
-        <a href="">Theme Variables</a>
-        <a href="">Responsive Design</a>
+        <Link to="/hover-state">Hover</Link>
+        <Link
+          className=" focus:bg-sky-600/30 focus:outline-1 rounded-full px-2 focus:outline-sky-500"
+          to="/responsive-design"
+        >
+          Responsive Design
+        </Link>
+        <Link to="">Focus</Link>
+        <Link to="">Dark Mode</Link>
+        <Link to="">Theme Variables</Link>
       </div>
       <button className=" bg-orange-700 h-3/5  py-4 flex flex-col items-center justify-center px-2 text-white outline-gray-200 shadow-gray-500 rounded-full">
-        All Docs
+        All Documentation
       </button>
     </div>
   );

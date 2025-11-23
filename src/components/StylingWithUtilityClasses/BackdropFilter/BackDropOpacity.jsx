7b6c1f36a1ed3783fe14fb2648a11bca2ce0820c
Kevin Langat <kevinlangat419@gmail.com>
2025-11-23 22:22:57 +0300

    Did backdrop-contrast, utility used to apply backdrop contrast filters to an element.Use utilities like backdrop-contrast-50 and backdrop-contrast-100 to control an element's backdrop contrast.

---
+++ src/components/StylingWithUtilityClasses/BackdropFilter/BackDropOpacity.jsx
@@ -0,0 +1,74 @@
+function BackDropOpacity() {
+  return (
+    <div className='w-full  mb-20 flex flex-col items-center justify-center gap-4 '>
+      <h2 className=' text-[medium] font-bold rounded-full bg-sky-600/30 outline-1 outline-sky-500  px-4'>
+        backdrop-filter: opacity()
+      </h2>
+      <h2 className='flex flex-col items-center text-vsm'>
+        Utilities for applying backdrop opacity filters to an element.
+      </h2>
+      {/* Basic example */}
+      <h2 className='underline'>Basic Example</h2>
+      <h2 className='text-vsm w-1/2'>
+        Use utilities like backdrop-opacity-50 and backdrop-opacity-75 to
+        control the opacity of all the backdrop filters applied to an element:
+      </h2>
+      <div className='w-3/5 outline-1 outline-gray-400/40  bg-gray-100 h-40 rounded-[0.3em] flex flex-row justify-around'>
+        <div className='  flex  flex-col items-center justify-around'>
+          <h2 className='text-vsm'>backdrop-opacity-10</h2>
+          <div className='relative w-full flex flex-col justify-center items-center'>
+            <img
+              src='/Images/abstract.png'
+              alt=''
+              className='w-45 h-30 rounded-[0.2em]'
+            />
+            <div className='w-3/4 backdrop-invert backdrop-opacity-10 bg-white/30 absolute h-16'></div>
+          </div>
+        </div>
+        <div className='  flex  flex-col items-center justify-around'>
+          <h2 className='text-vsm'>backdrop-opacity-60</h2>
+          <div className='relative w-full flex flex-col justify-center items-center'>
+            <img
+              src='/Images/abstract.png'
+              alt=''
+              className='w-45 h-30 rounded-[0.2em]'
+            />
+            <div className='w-3/4 backdrop-invert backdrop-opacity-60 bg-white/30 absolute h-16'></div>
+          </div>
+        </div>
+        <div className='  flex  flex-col items-center justify-around'>
+          <h2 className='text-vsm'>backdrop-opacity-90</h2>
+          <div className='relative w-full flex flex-col justify-center items-center'>
+            <img
+              src='/Images/abstract.png'
+              alt=''
+              className='w-45 h-30 rounded-[0.2em]'
+            />
+            <div className='w-3/4 backdrop-invert backdrop-opacity-90 bg-white/30 absolute h-16'></div>
+          </div>
+        </div>
+      </div>
+      {/* Using a custom value */}
+      <h2 className='underline'>Using a custom value</h2>
+      <h2 className='text-vsm w-1/2'>
+        Use the backdrop-opacity-{'[<value>]'} syntax to set the backdrop
+        inversion based on a completely custom value:
+      </h2>
+      <div className='w-3/5 outline-1 outline-gray-400/40  bg-gray-100 h-40 rounded-[0.3em] flex flex-row justify-around'>
+        <div className='  flex  flex-col items-center justify-around'>
+          <h2 className='text-vsm'>backdrop-invert-[.75]</h2>
+          <div className='relative w-full flex flex-col justify-center items-center'>
+            <img
+              src='/Images/abstract.png'
+              alt=''
+              className='w-45 h-30 rounded-[0.2em]'
+            />
+            <div className='w-3/4 backdrop-invert backdrop-opacity-[.75] bg-white/30 absolute h-16'></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BackDropOpacity;
